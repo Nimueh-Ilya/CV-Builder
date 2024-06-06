7cad101c3e738182201f16afcc34fd94f3d2f1d4
Nimueh-Ilya <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-06-06 18:31:14 +0100

    chore : Add layout of the components

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -9,7 +9,31 @@ export default function MainContainer() {
         inputValue={inputValue}
         setInputValue={setInputValue}
       ></InputDetails>
-      <p>{inputValue}</p>
+      <div>
+        <section>
+          <h1>Name and Surname</h1>
+          <p>City,Country | phone-number | Email Address</p>
+        </section>
+        <section>
+          <h2>Summary</h2>
+          <div>'Separator'</div>
+          <p>Summary Text</p>
+        </section>
+        <section>
+          <h2>Education</h2>
+          <h3>School Name</h3>
+          <p>School text</p>
+        </section>
+        <section>
+          <h2>Experience</h2>
+          <div>'Separator'</div>
+          <p>Experience Component</p>
+        </section>
+        <section>
+          <h2>Skills</h2>
+          <p>Skills Component</p>
+        </section>
+      </div>
     </>
   );
 }
