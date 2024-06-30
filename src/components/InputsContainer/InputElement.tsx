7a4fa783efe6d3eceb571a1ee41d254d22047e60
Nimueh-Ilya <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-06-30 12:15:42 +0100

    feat :  Add a validation button

--- src/components/InputsContainer/InputElement.tsx
+++ src/components/InputsContainer/InputElement.tsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
+
 interface InputElementProps {
   label: string;
 }
 
-export function InputElement({ label }: InputElementProps) {
+export function InputElement({ label, childToParent }: InputElementProps) {
+  const [valid, setValid] = useState(false);
+  const [value, setValue] = useState("");
+  const validSwitch = () => {
+    setValid(!valid);
+  };
+  const inputChange = (inputVal: string) => {
+    setValue(inputVal);
+  };
   return (
     <div className="grow flex align-middle gap-1 ">
       <label className="self-center" htmlFor={label.split(" ").join("")}>
         {label}
       </label>
-      <input className="grow" id={label.split(" ").join("")} type="text" />
+      <input
+        onChange={(e) => inputChange(e.target.value)}
+        className="grow"
+        id={label.split(" ").join("")}
+        type="text"
+      />
+      <button onClick={validSwitch}>
+        {valid ? <div>✔️</div> : <div>✖️</div>}
+      </button>
     </div>
   );
 }
